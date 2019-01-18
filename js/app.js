d18245116b37b9381ced9b2e82a069c58606351f
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-18 12:31:27 -1000

    app.js setup, addToShoppingList() method

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const shopping_list = require('../js/shopping-list.js');
+const shopping_list_item = require('../js/shopping-list-item.js');
+const ShoppingList = shopping_list.ShoppingList;
+const ShoppingListItem = shopping_list_item.ShoppingListItem;
+
+const content = document.querySelector('#content');
+const addItemButton = document.querySelector('#addShoppingListItemButton');
+
+const list = new ShoppingList();
+
+function addToShoppingList() {
+  const title = document.querySelector('#title').value;
+  const description = document.querySelector('#description').value;
+  const newItem = new ShoppingListItem(title, description);
+
+  list.addItem(newItem);
+}
+
+function addToShoppingList() {
+  
+}
+
+addItemButton.addEventListener('click', addToShoppingList);
+
+const listElement = ShoppingList.render();
+
+content.innerHTML = listElement;
