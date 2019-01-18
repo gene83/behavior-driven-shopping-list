842af54fbc7e5eba4f1c653f8446aa8e20834a23
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-17 19:54:15 -1000

    tests if ShoppingList is a class

--- test/shopping-list-item.test.js
+++ test/shopping-list-item.test.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const chai = require('chai');
-const shoppingJS = require('../js/shopping-list-item.js');
-const ShoppingListItem = shoppingJS.ShoppingListItem;
+const shopping_list_item = require('../js/shopping-list-item.js');
+const shopping_list = require('../js/shopping-list.js');
+const ShoppingListItem = shopping_list_item.ShoppingListItem;
+const ShoppingList = shopping_list.ShoppingList;
 
 const expect = chai.expect;
 const should = chai.should;
 
 console.log(shoppingJS);
 
-describe('ShoppingListItem', function() {
-  it('should be a class', function() {
+describe('ShoppingListItem', function () {
+  it('should be a class', function () {
     expect(ShoppingListItem).to.be.a('function');
     expect(new ShoppingListItem()).to.be.instanceof(ShoppingListItem);
   });
 
-  it("should have property named 'name'", function() {
+  it("should have property named 'name'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni');
 
     expect(new ShoppingListItem()).to.have.property('name');
@@ -23,7 +25,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.name).to.deep.equal('pizza');
   });
 
-  it("should have property named 'description'", function() {
+  it("should have property named 'description'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni');
 
     expect(new ShoppingListItem()).to.have.property('description');
@@ -31,7 +33,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.description).to.deep.equal('pepperoni');
   });
 
-  it("should have property 'isDone'", function() {
+  it("should have property 'isDone'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni', 'third');
 
     expect(pizza).to.have.property('isDone');
@@ -39,7 +41,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.isDone).to.deep.equal(false);
   });
 
-  it('should have a constructor method that accepts 2 arguments, name and description', function() {
+  it('should have a constructor method that accepts 2 arguments, name and description', function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni', 'third');
 
     expect(pizza.name).to.deep.equal('pizza');
@@ -47,7 +49,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.third).to.be.undefined;
   });
 
-  it("should have a method named 'check'", function() {
+  it("should have a method named 'check'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni');
 
     expect(pizza.check).to.exist;
@@ -57,7 +59,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.isDone).to.deep.equal(true);
   });
 
-  it("should have a method named 'uncheck'", function() {
+  it("should have a method named 'uncheck'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni');
 
     expect(pizza.uncheck).to.exist;
@@ -68,7 +70,7 @@ describe('ShoppingListItem', function() {
     expect(pizza.isDone).to.deep.equal(false);
   });
 
-  it("should have a method named 'render'", function() {
+  it("should have a method named 'render'", function () {
     const pizza = new ShoppingListItem('pizza', 'pepperoni');
 
     expect(pizza.render).to.exist;
@@ -76,5 +78,12 @@ describe('ShoppingListItem', function() {
     expect(pizza.render()).to.deep.equal(
       '<li class="completed_false"><span>pizza</span> <span>pepperoni</span></li>'
     );
+  });
+});
+
+describe('ShoppingList', function () {
+  it('should be a class', function () {
+    expect(ShoppingList).to.be.a('function');
+    expect(new ShoppingList()).to.be.instanceof(ShoppingList);
   });
 });
